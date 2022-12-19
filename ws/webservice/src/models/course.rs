fd0ceb38449ffff01300516d2d805883b96a295f
lc-1010 <532398960@qq.com>
2022-12-19 13:41:37 +0800

    updated

--- ws/webservice/src/models/course.rs
+++ ws/webservice/src/models/course.rs
@@ -4,12 +4,21 @@ use serde::{Deserialize, Serialize};
 
 // use crate::models::course::Course;
 
-#[derive(Deserialize, Serialize, Debug, Clone )]
+#[derive(Deserialize, Serialize, Debug, Clone, sqlx::FromRow )]// sqlx::FromRow 自动映射
 pub struct Course {
     pub teacher_id: usize,
     pub id:Option<usize>,//可空
     pub name: String,
     pub time: Option<NaiveDateTime>,//可空
+
+    pub description: Option<String>,
+    pub format: Option<String>,
+    pub structture: Option<String>,
+    pub duration: Option<String>,
+    pub price: Option<i32>,
+    pub language: Option<String>,
+    pub level: Option<String>,
+
 }
 
 
